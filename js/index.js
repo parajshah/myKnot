8d24276aa3311e1b72cd7e88fa7e7ba61fff7bcd
Paraj Shah <parajshah2000@gmail.com>
2020-11-28 19:49:04 +0530

    Completely changed UI Design, Added Service page
    Added new page for services
    Added about section
    Added footer

--- js/index.js
+++
@@ -1,8 +0,0 @@
-
-// Change colour of navbar on scroll
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".navbar");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-  });
